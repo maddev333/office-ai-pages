7970a5cd3a3ab8e93f1e781f000d80fcaf78b00c
maddev333 <maddev333@users.noreply.github.com>
2026-05-30 18:21:11 +0000

    deploy: maddev333/map-excel-azure@d3a85e7ca21cafe5ffce09212f4e4e2bf558b9c9

---
+++ assets/index-CmE7A6O7.js
@@ -0,0 +1,2 @@
+import{_ as r}from"./taskpane-B2EZ66Iv.js";import"./modulepreload-polyfill-B5Qt9EMX.js";var _={type:"sync",importFFI:()=>r(()=>import("./ffi-Boa1QuFa.js"),[]).then(t=>t.QuickJSFFI),importModuleLoader:()=>r(()=>import("./emscripten-module.browser-9rx1fijm.js"),[]).then(t=>t.default)},i=_;export{i as default};
+//# sourceMappingURL=index-CmE7A6O7.js.map
